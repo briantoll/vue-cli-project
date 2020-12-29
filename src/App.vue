13037352f58240ee77bb096889bf37a91a0ba0eb
Brian Toll <briantoll@fastmail.com>
2020-12-29 11:31:09 -0500

    add form with error checking dialog to add new items

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,19 @@
 <template>
   <the-header title="Resources"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-resources></the-resources>
+  
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
+
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 
 export default {
   components: {
-    StoredResources,
-    TheHeader
-  },
-  data() {
-    return {
-      storedResources: [
-        { 
-          id: 'official-guide', 
-          title: 'Official Guide', 
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org'
-        },
-        { 
-          id: 'duckduckgo', 
-          title: 'DuckDuckGo', 
-          description: 'The best search engine',
-          link: 'https://duckduckgo.com'
-        },
-      ]
-    };
+    TheHeader,
+    TheResources
   }
 }
 </script>
---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,78 @@
+<template>
+  <base-card>
+    <base-button 
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResourcesButtonMode"
+      >
+      Stored Resources
+    </base-button>
+    <base-button 
+      @click="setSelectedTab('add-resource')"
+      :mode="addResourceButtonMode">
+      Add Resource
+    </base-button>
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        { 
+          id: 'official-guide', 
+          title: 'Official Guide', 
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        { 
+          id: 'duckduckgo', 
+          title: 'DuckDuckGo', 
+          description: 'The best search engine',
+          link: 'https://duckduckgo.com'
+        },
+      ]
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource
+    };
+  },
+  computed: {
+    storedResourcesButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResourceButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: link
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
+    }
+  }
+}
+</script>
